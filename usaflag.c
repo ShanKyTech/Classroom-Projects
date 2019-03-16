b6ecb1d50f41be3f7461bd5502eb6fdb887ac998
Shankar Gurung <admin@shankytech.com>
2019-03-15 20:00:13 -0400

    usaflag.c

---
+++ usaflag.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int a =5 ;
+   
+ while (a--> 0) // outer while  loop
+ {
+     int b =5;
+     int c=10;
+     if (a%2==0) printf(" \t");
+     while(b >0) // inner while loop
+     {
+         if(b%2==0);
+         {
+         printf("* \t");
+         }
+        // else
+         {
+           //  printf("=");
+         }
+         b=b-1;
+     }
+      while(c-- >0) // inner while loop
+     {
+          printf("=");
+     }
+     printf("\n");
+ }
+    return 0;
+}
